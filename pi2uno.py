84f196dffd109b306af805852bffeddd9c1ae062
Nikolaus Piccolotto <prayerslayer@gmail.com>
2013-11-29 19:37:18 +0000

    setting volume based on message from arduino

--- pi2uno.py
+++ pi2uno.py
@@ -1,9 +1,49 @@
+# arduino serial connection
 import serial
+
+# regexes
+import re
+
+# mpd library
+from mpd import MPDClient
+
+# ==== UTILS =====
+
+NMBR_REGEX  = re.compile( "\D*(\d{1,3})" ) # anything but a digit with 1 to 3 digits at the end
+
+# ==== SETUP =====
 
 # connect to arduino uno
 uno = serial.Serial( "/dev/ttyACM0", 9600 )
 
+# connect to mpd
+mop = MPDClient()
+mop.connect( "localhost", 6600 ) # default port
+
+# ==== FUNCTIONS ====
+
+def processCommand( input ):
+	# if command doesn't start with CMD return
+	if not( input.startswith( "CMD" ) ):
+		return
+	# check if it's SET VOLUME
+	if input.startswith( "CMD SET VOLUME" ):
+		# match number regex on command
+		vol_result = NMBR_REGEX.findall( input )
+		# get first match,convert to int
+		vol = int( vol_result[ 0 ] )
+		# update mpd
+		mop.setvol( vol )
+		print "DEBUG set volume to " + str( vol )
+
+# ==== LOOP ====
+
 # there really is no keyword for true in python 2
-# and this sentence rhymes.
-while 1 : 
-    print uno.readline()
+command = "noop"
+while 1:
+	# read serial from arduino
+	command = uno.readline()
+	print "DEBUG: " + command
+	# repeat if there is no command
+	if len( command ) > 0:
+		processCommand( command )
