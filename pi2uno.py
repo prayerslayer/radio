2f149729f7b366df5968968fa8a6aaa6601686c6
Nikolaus Piccolotto <prayerslayer@gmail.com>
2013-12-22 15:55:12 +0000

    play first playlist automatically, establish communication to arduino

--- pi2uno.py
+++ pi2uno.py
@@ -7,6 +7,9 @@ import re
 # mpd library
 from mpd import MPDClient
 
+# sleep function
+from time import sleep
+
 # ==== UTILS =====
 
 NMBR_REGEX  = re.compile( "\D*(\d{1,3})" ) # anything but a digit with 1 to 3 digits at the end
@@ -14,11 +17,25 @@ NMBR_REGEX  = re.compile( "\D*(\d{1,3})" ) # anything but a digit with 1 to 3 di
 # ==== SETUP =====
 
 # connect to arduino uno
-uno = serial.Serial( "/dev/ttyACM0", 9600 )
+uno = serial.Serial( "/dev/ttyACM0", 9600, timeout=1 )
+
+# wait 5 sec
+sleep( 5 )
 
 # connect to mpd
 mop = MPDClient()
 mop.connect( "localhost", 6600 ) # default port
+
+# play first playlist
+currentPlaylist = 0
+playlists = map( lambda pl: pl[ "playlist" ], mop.listplaylists() )
+mop.load( playlists[ currentPlaylist] )
+
+# tell arduino we're ready
+uno.write( "CMD RUN\n\r" )
+
+sleep( 12 )
+mop.play( 0 )
 
 # ==== FUNCTIONS ====
 
@@ -40,6 +57,7 @@ def processCommand( input ):
 
 # there really is no keyword for true in python 2
 command = "noop"
+
 while 1:
 	# read serial from arduino
 	command = uno.readline()
