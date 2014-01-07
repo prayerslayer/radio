a21ee6c3dc5f7284c32e2606823003990ee4a972
Nikolaus Piccolotto <prayerslayer@gmail.com>
2014-01-07 22:55:08 +0100

    rotary encoder for volume

--- ardu_ino/ardu_ino.ino
+++ ardu_ino/ardu_ino.ino
@@ -1,13 +1,22 @@
 #include "math.h"
 
+int STATION_PIN = A0;
 int STATUS_LED_PIN = 13;
-int STATION_PIN = A1;  // pin to control radio stations from given source
-int VOLUME_PIN = A0;  // pin with cable for volume potentiometer
+int VOLUME_UP_PIN = 2;
+int VOLUME_DN_PIN = 3;
 int MAX_VOLUME = 100;
 
 int DEVICE_STATUS = -1;
 int DEVICE_STATUS_WAIT = 0;  // means status led should blink. it's 1 if it's fine and up
 int DEVICE_STATUS_UP = 1;    // led is on
+
+volatile int vol_seqstore = 0;
+volatile int vol_encoded = 0b11;
+volatile int vol_lastEncoded = 0;
+volatile long vol_encoderValue = 0;
+volatile long vol_lastEncoderValue = 0;
+volatile int vol_MSB = 1;
+volatile int vol_LSB = 1;
 
 int lastVolume = 70;
 int currentVolume = 70;  //TODO this should be set in setup from the pi
@@ -58,9 +67,18 @@ void processSerial() {
 }
 
 void readVolume() {
-  float vol = analogRead( VOLUME_PIN );
-  currentVolume = floor( vol / 1023.0 * 100 );  // convert to percentage
-  currentVolume = currentVolume - ( currentVolume % 10 ); // round down to next 10-step
+  if ( currentVolume == vol_encoderValue ) {
+    return;
+  }
+  if ( vol_encoderValue >= 100 ) {
+    currentVolume = 100;
+    return;
+  } 
+  if ( vol_encoderValue <= 0 ) {
+    currentVolume = 0;
+    return;
+  }
+  currentVolume = vol_encoderValue;
 }
 
 void readStation() {
@@ -78,13 +96,59 @@ void setup() {
   Serial.begin( 9600 );
   // set pin modes
   pinMode( STATUS_LED_PIN, OUTPUT );
+  pinMode( VOLUME_UP_PIN, INPUT );
+  pinMode( VOLUME_DN_PIN, INPUT );
+  digitalWrite( VOLUME_DN_PIN, HIGH );
+  digitalWrite( VOLUME_UP_PIN, HIGH );
+  
+  // ints
+  attachInterrupt( 0, updateVolume, CHANGE );
+  attachInterrupt( 1, updateVolume, CHANGE );
+  
   DEVICE_STATUS = DEVICE_STATUS_WAIT;
+  
   
   Serial.println( "Running internal setup" );
   // do some stuff?
   Serial.println( "Finished internal setup" );
   
   digitalWrite( STATUS_LED_PIN, HIGH );
+}
+
+/*
+*  Apparently the encoder I use produces 11 01 00 10 for one step.
+*  So I have to check for 11 01 00 10 or 01 00 10 11 pattern
+*/
+void updateVolume() {
+  vol_MSB = digitalRead( VOLUME_UP_PIN );
+  vol_LSB = digitalRead( VOLUME_DN_PIN );
+  
+  vol_encoded = ( vol_MSB << 1 ) | vol_LSB; //converting the 2 pin value to single number
+
+  if ( ( vol_seqstore & 0x3 ) != vol_encoded ) { 
+    // at least one of the bits has changed compared to last stable state
+    // (interrupt might bounce )
+    vol_seqstore = vol_seqstore << 2; //shift the next sequence step
+    vol_seqstore |= vol_encoded; // add encoded value
+    vol_seqstore = vol_seqstore & 0b11111111; // only keep last 8 bits
+    
+    vol_lastEncoderValue = vol_encoderValue;
+    
+    // counter-clockwise code is 10 00 01 11 (135) and all its permutations:
+    // 11 10 00 01 = 225
+    // 01 11 10 00 = 120
+    // 00 01 11 10 =  30
+    if ( vol_seqstore == 135 || vol_seqstore == 225 || vol_seqstore == 120 || vol_seqstore == 30  ) {
+      vol_encoderValue--;
+    }
+    // clockwise code is 01 00 10 11 (75) and all its permutations:
+    // 00 10 11 01 =  45
+    // 10 11 01 00 = 180
+    // 11 01 00 10 = 210
+    if ( vol_seqstore == 75 || vol_seqstore == 45 || vol_seqstore == 180 || vol_seqstore == 210 ) {
+      vol_encoderValue++;
+    }
+  }
 }
 
 void loop() {
